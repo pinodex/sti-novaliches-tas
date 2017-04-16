0866d23a9b0eac35fffd96ecce5e089ef6d0e669
Raphael Marco <raphaelmarco@outlook.com>
2017-04-16 21:21:31 +0800

    Minimize side menu

--- config/menu.php
+++ config/menu.php
@@ -3,134 +3,150 @@
 use App\Components\Acl;
 
 return [
-    'top' => [
+    'user' => [
+        'top' => [
 
+        ],
+
+        'side' => [
+            [
+                'header' => null,
+                
+                'list' => [
+                    [
+                        'icon'          => 'home',
+                        'identifier'    => 'home',
+                        'route'         => 'dashboard.index',
+                        'name'          => 'Home'
+                    ],
+
+                    [
+                        'icon'          => 'envelope',
+                        'identifier'    => 'requests',
+                        'route'         => 'dashboard.requests.index',
+                        'name'          => 'Requests',
+                        'visibility'    => [ Acl::MANAGE_REQUESTS, Acl::SUBMIT_REQUESTS ],
+
+                        'list' => [
+                            [
+                                'icon'          => 'plus',
+                                'identifier'    => 'create',
+                                'route'         => 'dashboard.requests.create',
+                                'name'          => 'Create Request',
+                                'visibility'    => [ Acl::SUBMIT_REQUESTS ]
+                            ],
+
+                            [
+                                'icon'          => 'user-circle-o',
+                                'identifier'    => 'me',
+                                'route'         => 'dashboard.requests.me',
+                                'name'          => 'My Requests'
+                            ]
+                        ]
+                    ],
+
+                    [
+                        'icon'          => 'sticky-note-o',
+                        'identifier'    => 'bulletin',
+                        'route'         => 'dashboard.bulletins.index',
+                        'name'          => 'Bulletin',
+                        'visibility'    => [ Acl::MANAGE_BULLETIN ]
+                    ],
+                ]
+            ],
+
+            [
+                'header'        => 'Administrator',
+                'visibility'    => [ Acl::MANAGE_USERS, Acl::MANAGE_GROUPS, Acl::MANAGE_DEPARTMENTS ],
+
+                'list' => [
+                    [
+                        'icon'          => 'user',
+                        'identifier'    => 'users',
+                        'route'         => 'dashboard.users.index',
+                        'name'          => 'Users',
+                        'visibility'    => [ Acl::MANAGE_USERS ]
+                    ],
+
+                    [
+                        'icon'          => 'users',
+                        'identifier'    => 'groups',
+                        'route'         => 'dashboard.groups.index',
+                        'name'          => 'Groups',
+                        'visibility'    => [ Acl::MANAGE_GROUPS ]
+                    ],
+
+                    [
+                        'icon'          => 'building',
+                        'identifier'    => 'departments',
+                        'route'         => 'dashboard.departments.index',
+                        'name'          => 'Departments',
+                        'visibility'    => [ Acl::MANAGE_DEPARTMENTS ]
+                    ],
+
+                    [
+                        'icon'          => 'address-book',
+                        'identifier'    => 'employees',
+                        'route'         => 'dashboard.employees.index',
+                        'name'          => 'Employees',
+                        'visibility'    => [ Acl::MANAGE_EMPLOYEES ],
+                    ]
+                ]  
+            ],
+
+            [
+                'header' => 'Account',
+
+                'list' => [
+                    [
+                        'icon'          => 'cog',
+                        'identifier'    => 'settings',
+                        'route'         => 'account.settings.index',
+                        'name'          => 'Account Settings'
+                    ]
+                ]  
+            ]
+        ]
     ],
 
-    'side' => [
-        [
-            'header' => null,
-            
-            'list' => [
-                [
-                    'icon'          => 'home',
-                    'identifier'    => 'home',
-                    'route'         => 'dashboard.index',
-                    'name'          => 'Home'
+    'employee' => [
+        'top' => [
+
+        ],
+
+        'side' => [
+            [
+                'header' => null,
+                
+                'list' => [
+                    [
+                        'icon'          => 'home',
+                        'identifier'    => 'home',
+                        'route'         => 'employee.index',
+                        'name'          => 'Home'
+                    ],
+
+                    [
+                        'icon'          => 'envelope',
+                        'identifier'    => 'requests',
+                        'route'         => 'dashboard.requests.index',
+                        'name'          => 'Requests',
+                    ],
                 ],
+            ],
 
-                [
-                    'icon'          => 'envelope',
-                    'identifier'    => 'requests',
-                    'route'         => 'dashboard.requests.index',
-                    'name'          => 'Requests',
-                    'visibility'    => [ Acl::MANAGE_REQUESTS, Acl::SUBMIT_REQUESTS ],
+            [
+                'header' => 'Account',
 
-                    'list' => [
-                        [
-                            'icon'          => 'plus',
-                            'identifier'    => 'create',
-                            'route'         => 'dashboard.requests.create',
-                            'name'          => 'Create Request',
-                            'visibility'    => [ Acl::SUBMIT_REQUESTS ]
-                        ],
-
-                        [
-                            'icon'          => 'user-circle-o',
-                            'identifier'    => 'me',
-                            'route'         => 'dashboard.requests.me',
-                            'name'          => 'My Requests'
-                        ]
+                'list' => [
+                    [
+                        'icon'          => 'cog',
+                        'identifier'    => 'settings',
+                        'route'         => 'account.settings.index',
+                        'name'          => 'Account Settings'
                     ]
-                ],
-
-                [
-                    'icon'          => 'sticky-note-o',
-                    'identifier'    => 'bulletin',
-                    'route'         => 'dashboard.bulletins.index',
-                    'name'          => 'Bulletin',
-                    'visibility'    => [ Acl::MANAGE_BULLETIN ]
-                ],
+                ]  
             ]
-        ],
-
-        [
-            'header'        => 'Administrator',
-            'visibility'    => [ Acl::MANAGE_USERS, Acl::MANAGE_GROUPS, Acl::MANAGE_DEPARTMENTS ],
-
-            'list' => [
-                [
-                    'icon'          => 'user',
-                    'identifier'    => 'users',
-                    'route'         => 'dashboard.users.index',
-                    'name'          => 'Users',
-                    'visibility'    => [ Acl::MANAGE_USERS ],
-                    
-                    'list' => [
-                        [
-                            'icon'          => 'trash',
-                            'identifier'    => 'deleted',
-                            'route'         => 'dashboard.users.deleted',
-                            'name'          => 'Deleted Users'
-                        ]
-                    ]
-                ],
-
-                [
-                    'icon'          => 'users',
-                    'identifier'    => 'groups',
-                    'route'         => 'dashboard.groups.index',
-                    'name'          => 'Groups',
-                    'visibility'    => [ Acl::MANAGE_GROUPS ],
-
-                    'list' => [
-                        [
-                            'icon'          => 'trash',
-                            'identifier'    => 'deleted',
-                            'route'         => 'dashboard.groups.deleted',
-                            'name'          => 'Deleted Groups'
-                        ]
-                    ]
-                ],
-
-                [
-                    'icon'          => 'building',
-                    'identifier'    => 'departments',
-                    'route'         => 'dashboard.departments.index',
-                    'name'          => 'Departments',
-                    'visibility'    => [ Acl::MANAGE_DEPARTMENTS ],
-
-                    'list' => [
-                        [
-                            'icon'          => 'trash',
-                            'identifier'    => 'deleted',
-                            'route'         => 'dashboard.departments.deleted',
-                            'name'          => 'Deleted Departments'
-                        ]
-                    ]
-                ],
-
-                [
-                    'icon'          => 'address-book',
-                    'identifier'    => 'employees',
-                    'route'         => 'dashboard.employees.index',
-                    'name'          => 'Employees',
-                    'visibility'    => [ Acl::MANAGE_EMPLOYEES ],
-                ]
-            ]  
-        ],
-
-        [
-            'header' => 'Account',
-
-            'list' => [
-                [
-                    'icon'          => 'cog',
-                    'identifier'    => 'settings',
-                    'route'         => 'account.settings.index',
-                    'name'          => 'Account Settings'
-                ]
-            ]  
         ]
     ]
 ];
