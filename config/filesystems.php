aaf7275b9bc1aab85268473fd2f8a056125a89c8
Raphael Marco <raphaelmarco@outlook.com>
2017-07-08 09:38:42 +0800

    Enable Travis on master

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,12 +45,12 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => env('DISK_LOCAL_PATH', storage_path('app')),
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => env('DISK_PUBLIC_PATH', storage_path('app/public')),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
